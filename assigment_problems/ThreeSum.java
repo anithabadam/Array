d7415a738d2bd12a62eb60cb36ba889c67c6363c
NagaMani <@anithabadam.com>
2026-09-18 22:52:13 +0530

    Added ThreeSum assignment

---
+++ assigment_problems/ThreeSum.java
@@ -0,0 +1,48 @@
+package assigment_problems;
+
+import java.util.Arrays;
+
+public class ThreeSum {
+
+    public static void threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    System.out.println(
+                        "[" + nums[i] + ", " + nums[left] + ", " + nums[right] + "]"
+                    );
+
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+                    left++;
+
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        threeSum(nums);
+    }
+}
